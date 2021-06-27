0668ff388d354b5c92aa1e5cb5fa9aff4edc115c
Matej Balantič <matej@balantic.si>
2021-06-27 17:41:08 +0200

    Target last known revision that still works with our project

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["AudioKitUI"]),
     ],
     dependencies: [
-	.package(url: "https://github.com/AudioKit/AudioKit.git", from: "v5.0beta1")
+	.package(url: "https://github.com/AudioKit/AudioKit.git", .revision("b1099859f6654c98f0f28e9fb1ed388ae96cb7d6"))
     ],
     targets: [
         .target(
